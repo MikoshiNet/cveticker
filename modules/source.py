f72bb0f691ce01878e32794039848d44c5e2c94d
chaeynz <kevin@danielpuls.com>
2023-12-14 07:57:40 +0100

    source: Bugfix and removed nested functions The functions for fetching and processing feeds and entries were nested in "fetch_and_process_feed". These are now seperate functions
    The check if a url was already existing in the dataset was missing "not" in the if statement. This was changed

--- modules/source.py
+++ modules/source.py
@@ -18,39 +18,39 @@ def strip_html_tags(html):
     text = soup.get_text(separator=' ')
     return text
 
+async def process_entry(session, dataset, entry):
+    content_html = await fetch_entry(session, entry)
+    if content_html is not None:
+        cves = deduce_cve(content_html)
+        content_raw = strip_html_tags(content_html)
+        dataset.add(Data(entry.link, content_raw, cves))
+
+async def fetch_feed(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.text()
+            feed = feedparser.parse(content)
+            return feed
+
+async def fetch_entry(session, entry) -> str:
+    try:
+        async with session.get(entry.link) as response:
+            response.raise_for_status()
+            return await response.text()
+    except aiohttp.ClientError as e:
+        print(f"Client error occurred: {e}")
+        return None
+    except aiohttp.HttpProcessingError as e:
+        print(f"HTTP processing error occurred: {e}")
+        return None
+    except Exception as e: # pylint: disable=broad-exception-caught
+        print(f"An unexpected error occurred: {e}")
+        return None
+
 async def fetch_and_process_feed(session, url, dataset:Dataset):
-    async def process_entry(entry):
-        content_html = await fetch_entry(entry)
-        if content_html is not None:
-            cves = deduce_cve(content_html)
-            content_raw = strip_html_tags(content_html)
-            dataset.add(Data(entry.link, content_raw, cves))
-
-    async def fetch_feed(url):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                content = await response.text()
-                feed = feedparser.parse(content)
-                return feed
-
-    async def fetch_entry(entry) -> str:
-        try:
-            async with session.get(entry.link) as response:
-                response.raise_for_status()
-                return await response.text()
-        except aiohttp.ClientError as e:
-            print(f"Client error occurred: {e}")
-            return None
-        except aiohttp.HttpProcessingError as e:
-            print(f"HTTP processing error occurred: {e}")
-            return None
-        except Exception as e: # pylint: disable=broad-exception-caught
-            print(f"An unexpected error occurred: {e}")
-            return None
-
     feed:feedparser.FeedParserDict = await fetch_feed(url)
     for entry in feed.entries:
-        if entry.link in dataset:
-            await process_entry(entry)
+        if entry.link not in dataset:
+            await process_entry(session, dataset, entry)
         else:
             continue
